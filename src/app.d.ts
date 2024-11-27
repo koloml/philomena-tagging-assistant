c7f40e99b780b568501916b7f4881e97a28b52e2
KoloMl <thekolomoets@hotmail.com>
2024-11-28 00:43:55 +0400

    Added mapping of entities and their names

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,24 +1,30 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import MaintenanceProfile from "$entities/MaintenanceProfile.ts";
+
 declare global {
-    namespace App {
-        // interface Error {}
-        // interface Locals {}
-        // interface PageData {}
-        // interface PageState {}
-        // interface Platform {}
-        type LinkTarget = "_blank" | "_self" | "_parent" | "_top";
-        type IconName = (
-                "tag"
-                | "paint-brush"
-                | "arrow-left"
-                | "info-circle"
-                | "wrench"
-                | "globe"
-                | "plus"
-                | "file-export"
-                );
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+    type LinkTarget = "_blank" | "_self" | "_parent" | "_top";
+    type IconName = (
+      "tag"
+      | "paint-brush"
+      | "arrow-left"
+      | "info-circle"
+      | "wrench"
+      | "globe"
+      | "plus"
+      | "file-export"
+      );
+
+    interface EntityNamesMap {
+      profiles: MaintenanceProfile;
     }
+  }
 }
 
 export {};
