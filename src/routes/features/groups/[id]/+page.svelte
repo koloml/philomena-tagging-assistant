4fcf83d732ad006d38b6b7758bb357b570b06345
KoloMl <thekolomoets@hotmail.com>
2024-12-02 03:23:32 +0400

    Group detail view & editing form

---
+++ src/routes/features/groups/[id]/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import { goto } from "$app/navigation";
+    import { page } from "$app/stores";
+    import GroupView from "$components/features/GroupView.svelte";
+    import Menu from "$components/ui/menu/Menu.svelte";
+    import MenuItem from "$components/ui/menu/MenuItem.svelte";
+    import { tagGroupsStore } from "$stores/tag-groups-store.js";
+
+    const groupId = $page.params.id;
+    /** @type {import('$entities/TagGroup.ts').default|null} */
+    let group = null;
+
+    if (groupId==='new') {
+        goto('/features/groups/new/edit');
+    }
+
+    $: {
+        group = $tagGroupsStore.find(group => group.id===groupId) || null;
+
+        if (!group) {
+            console.warn(`Group ${ groupId } not found.`);
+            goto('/features/groups');
+        }
+    }
+</script>
+
+<Menu>
+    <MenuItem href="/features/groups" icon="arrow-left">Back</MenuItem>
+    <hr>
+</Menu>
+{#if group}
+    <GroupView {group}/>
+{/if}
+<Menu>
+    <hr>
+    <MenuItem href="/features/groups/{groupId}/edit" icon="wrench">Edit Group</MenuItem>
+    <MenuItem href="/features/groups/{groupId}/export" icon="file-export">Export Group</MenuItem>
+</Menu>
