7559f0a8029611ca813b19517c91e9d8fe043a86
KoloMl <thekolomoets@hotmail.com>
2026-04-05 22:26:58 +0400

    Added avali head into the footer near the author name

--- src/components/layout/Footer.svelte
+++ src/components/layout/Footer.svelte
@@ -6,11 +6,13 @@
   <a href="https://github.com/koloml/furbooru-tagging-assistant/releases/tag/{version}" target="_blank">
     v{version}
   </a>
-  <span>, made with ♥ by KoloMl.</span>
+  <span>, made with ♥ by
+    {#if __CURRENT_SITE__ === 'furbooru'}<i class="head"></i>{/if} KoloMl.</span>
 </footer>
 
 <style lang="scss">
   @use '$styles/colors';
+  @use '$styles/environment';
 
   footer {
     display: flex;
@@ -27,6 +29,21 @@
       &:hover {
         text-decoration: underline;
       }
+    }
+  }
+
+  @if environment.$current-site == 'furbooru' {
+    .head {
+      content: '';
+      height: 24px;
+      width: 32px;
+      display: inline-block;
+      vertical-align: middle;
+      background: {
+        image: url(/src/assets/heads/avali-optimized.svg);
+        size: contain;
+        repeat: no-repeat;
+      };
     }
   }
 </style>
